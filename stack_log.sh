2d3d65323e5b683cabac8f66daea9bbc9e30a458
MFuentes1992 <markfuentes2991@gmail.com>
2021-03-16 15:38:16 -0600

    Adding subfolder behaviour

--- stack_log.sh
+++ stack_log.sh
@@ -2,12 +2,17 @@
 CLUSTER='3'
 ENV='PROD'
 CUSTOMER='2287'
-RULESHEET='InvoiceRouting'
+RULESHEET='SubmitCompliance'
 CYEAR=`date '+%Y'`
 CMONTH=`date '+%m'`
 CDAY=`date '+%d'`
 CHOUR=`date '+%H'`
 CMINUTE=`date '+%M'`
-
-printf "*%s*/%s/-%s-,%s,@%s@&%s&?%s?+%s+:%s:%s\n" "$CLUSTER" "$ENV" "$CUSTOMER" "$RULESHEET" "$CYEAR" "$CMONTH" "$CDAY" "$CHOUR" "$CMINUTE" >> ../Jenkins/log_stack
+FILE="c$CLUSTER/"
+if [ "$ENV" == "PROD" ]; then
+    FILE="c$CLUSTER/logs_prod.txt"
+else
+    FILE="c$CLUSTER/logs_qa.txt"
+fi
+printf "/%s/,%s,@%s@&%s&?%s?+%s+:%s:\n" "$CUSTOMER" "$RULESHEET" "$CYEAR" "$CMONTH" "$CDAY" "$CHOUR" "$CMINUTE" >> ../Jenkins/"$FILE"
 #echo "\n*$CLUSTER*/$ENV/-$CUSTOMER-,$RULESHEET,%$CYEAR%&$CMONTH&+$CDAY+@$CHOUR@:$CMINUTE:" >> ../Jenkins/log_stack
