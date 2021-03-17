14ccee9b32f12a61ee74325b49deb0db52d60118
Marco A. Fuentes <31914202+MFuentes1992@users.noreply.github.com>
2021-03-17 11:59:36 -0600

    Normalizing Variable names

--- stack_log.sh
+++ stack_log.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 CLUSTER='3'
-ENV='PROD'
-CUSTOMER='2287'
-RULESHEET='SubmitCompliance'
+ENVIRONMENT='PROD'
+CustomerID='2287'
+RuleType='SubmitCompliance'
 CYEAR=`date '+%Y'`
 CMONTH=`date '+%m'`
 CDAY=`date '+%d'`
 CHOUR=`date '+%H'`
 CMINUTE=`date '+%M'`
 FILE="c$CLUSTER/"
-if [ "$ENV" == "PROD" ]; then
-    FILE="c$CLUSTER/logs_prod.txt"
-else
-    FILE="c$CLUSTER/logs_qa.txt"
-fi
-printf "/%s/,%s,@%s@&%s&?%s?+%s+:%s:\n" "$CUSTOMER" "$RULESHEET" "$CYEAR" "$CMONTH" "$CDAY" "$CHOUR" "$CMINUTE" >> ../Jenkins/"$FILE"
+FILE="c$CLUSTER/logs_$ENVIRONMENT.txt"
+printf "/%s/,%s,@%s@&%s&?%s?+%s+:%s:\n" "$CustomerID" "$RuleType" "$CYEAR" "$CMONTH" "$CDAY" "$CHOUR" "$CMINUTE" >> ../Jenkins/"$FILE"
 #echo "\n*$CLUSTER*/$ENV/-$CUSTOMER-,$RULESHEET,%$CYEAR%&$CMONTH&+$CDAY+@$CHOUR@:$CMINUTE:" >> ../Jenkins/log_stack
